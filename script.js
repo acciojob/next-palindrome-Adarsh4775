fb9af49798de49f045561a2f188502bebc5f6fbd
Adarsh4775 <137145969+Adarsh4775@users.noreply.github.com>
2023-11-03 18:47:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
-function nextPalindrome(num) {
-  //your JS code here.
-}
 
+  //your JS code here.
+	 function nextPalindrome(num) {
+  function isPalindrome(n) {
+    return n.toString() === n.toString().split("").reverse().join("");
+  }
+
+  while (!isPalindrome(++num)) {}
+  return num;
+}
+ 
 const input = prompt("Enter a palindrome number");
-alert(nextPalindrome(input));
+alert(nextPalindrome(input)); 
+
+
+   
